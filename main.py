11112c61c0c0d9f0160472e98ef8690920b1ceb8
mirai-prog <pankkkou@icloud.com>
2024-11-25 16:30:51 +0100

    Commit

--- main.py
+++ main.py
@@ -4,22 +4,30 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+
 
 print('[INFO] Red led on')
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-
-print('[INFO] Red led off')
-GPIO.output(18, GPIO.LOW)
-
-GPIO.setup(17,GPIO.OUT)
-
-print('[INFO] Yellow led on')
 GPIO.output(17,GPIO.HIGH)
 time.sleep(1)
-
-print('[INFO] Yellow led off')
+print('[INFO] Red led off')
 GPIO.output(17, GPIO.LOW)
+
+#------
+GPIO.setup(27,GPIO.OUT)
+print('[INFO] Yellow led on')
+GPIO.output(27,GPIO.HIGH)
+time.sleep(1)
+print('[INFO] Yellow led off')
+GPIO.output(27, GPIO.LOW)
+
+#--------
+GPIO.setup(27,GPIO.OUT)
+print('[INFO] Yellow led on')
+GPIO.output(27,GPIO.HIGH)
+time.sleep(1)
+print('[INFO] Yellow led off')
+GPIO.output(27, GPIO.LOW)
 
